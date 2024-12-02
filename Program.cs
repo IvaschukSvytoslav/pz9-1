4487c3323a855985f2fe68169904e3110794f371
user <user@WINDOWS-5AN5BRH>
2024-12-02 20:07:26 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Введіть значення x:");
+        double x = Convert.ToDouble(Console.ReadLine());
+
+        // Лямбда-вирази
+        Func<double, double> positiveF = x => Math.Cos(x + 1);
+        Func<double, double> nonPositiveF = x => 1 - 2 * Math.Sin(x);
+
+        // Обчислення результату
+        double result = x > 0 ? positiveF(x) : nonPositiveF(x);
+
+        Console.WriteLine($"F({x}) = {result}");
+    }
+}
